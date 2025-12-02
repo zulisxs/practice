6f2c4019dd81b49f1a6bd616b5cfc31a630dd456
zulisxs <zulisxs021380@gmail.com>
2025-12-02 14:04:01 -0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,2 @@
+local Fluent, SaveManager, InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/Beta.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zulisxs/practice/refs/heads/main/ui.lua"))()
