0cee15ba889c4d7082ac59ebc6d33e694a56d056
zulisxs <zulisxs021380@gmail.com>
2025-12-03 15:13:34 -0300

    Delete main.lua

--- main.lua
+++
@@ -1,2 +0,0 @@
-local Fluent, SaveManager, InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/Beta.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zulisxs/practice/refs/heads/main/ui.lua"))()
