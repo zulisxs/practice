0823e1256cb5fbab03ce15ab47b7ac00055d991b
zulisxs <zulisxs021380@gmail.com>
2025-12-03 15:18:43 -0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,8 @@
+-- main.lua
+-- 1) Cargar FluentPlus
+local Fluent = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/discoart/FluentPlus/main/Beta.lua"
+))()
+
+-- 2) Cargar tu UI directamente desde GitHub
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zulisxs/practice/refs/heads/main/ui.lua"))()
